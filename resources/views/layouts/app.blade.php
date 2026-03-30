b85ef321f1c63b60787f15de8e7103cbdb54ba18
geeknow112 <geeknow112@gmail.com>
2026-03-30 23:04:25 +0900

    feat: e-Tax転記用画面（クリップボードコピー対応）

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,6 +21,7 @@
                 <a href="{{ route('depreciations.index') }}" class="hover:underline {{ request()->routeIs('depreciations.*') ? 'font-bold underline' : '' }}">減価償却</a>
                 <a href="{{ route('pl.index') }}" class="hover:underline {{ request()->routeIs('pl.*') ? 'font-bold underline' : '' }}">P/L</a>
                 <a href="{{ route('bs.index') }}" class="hover:underline {{ request()->routeIs('bs.*') ? 'font-bold underline' : '' }}">B/S</a>
+                <a href="{{ route('etax.index') }}" class="hover:underline {{ request()->routeIs('etax.*') ? 'font-bold underline' : '' }}">e-Tax転記</a>
                 <a href="{{ route('import.show') }}" class="hover:underline {{ request()->routeIs('import.*') ? 'font-bold underline' : '' }}">インポート</a>
                 <a href="{{ route('categories.index') }}" class="hover:underline {{ request()->routeIs('categories.*') ? 'font-bold underline' : '' }}">科目管理</a>
             </div>
